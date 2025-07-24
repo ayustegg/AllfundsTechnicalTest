1f034e6a7cdf4b0e7129d9245ccf37d9b7b30a5c
ayustegg <ayuste2410@gmail.com>
2025-07-24 11:55:58 +0200

    feat: add navigation menu and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import './App.css';
+
+import { NavigationMenu, NavigationMenuItem, NavigationMenuList, NavigationMenuLink } from './components/ui/navigation-menu';
+import { Home } from './pages/Home';
+import { News } from './pages/News';
+import { Archived } from './pages/Archived';
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <Link to="/news">News</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <Link to="/archived">Archived</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/archived" element={<Archived />} />
+        </Routes>
 
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/Archived.tsx
@@ -0,0 +1,3 @@
+export const Archived = () => {
+    return <div>Archived</div>;
+};
