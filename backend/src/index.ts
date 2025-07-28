8cfdd69d03a24d8ba9e7950abf2d93ac2dd60c25
ayustegg <victor.ayuste@gmail.com>
2025-07-28 16:01:11 +0200

    refactor(backend): format the document and organize

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,30 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import swaggerUi from 'swagger-ui-express';
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerOptions from '../swaggerOptions';
-import connectDB from "./config/mongo/db";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
 
+import swaggerOptions from "../swaggerOptions";
+import connectDB from "./config/mongo/db";
 import newsRouter from "./routes/news";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-connectDB();
+
 app.use(express.json());
 app.use(cors());
 const specs = swaggerJsdoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
+connectDB();
 app.get("/", (req: Request, res: Response) => {
-    res.send("Welcome to the Allfunds Tehcnical Test");
+  res.send("Welcome to the Allfunds Technical Test");
 });
 
 app.use("/api/news", newsRouter);
 
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 });
